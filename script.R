4d8eb57f9efc93ab9876462d600bdac559d2807d
Victor Hnidey <hnidey_13@hotmail.com>
2017-05-19 11:51:55 -0500

    added firsts script

---
+++ script.R
@@ -0,0 +1,23 @@
+args<-commandArgs(TRUE)
+
+var1<-as.numeric(args[1])
+var2<-as.numeric(args[2])
+ope<-as.character(args[3])
+
+if (ope=="sum") {
+resultado<-var1+var2
+}
+
+if (ope=="res") {
+resultado<-var1-var2
+}
+
+if (ope=="mul") {
+resultado<-var1*var2
+}
+
+if (ope=="div") {
+resultado<-var1/var2
+}
+
+resultado
